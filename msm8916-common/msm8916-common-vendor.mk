968b7c24698e253a561d9a347f671e6ea7bbbfa9
Kyle Harrison <khwebmail@gmail.com>
2018-09-06 14:13:40 +0100

    msm8916-common: Move bt blobs to vendor

--- msm8916-common/msm8916-common-vendor.mk
+++ msm8916-common/msm8916-common-vendor.mk
@@ -17,8 +17,8 @@
 
 PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/adsprpcd:vendor/bin/adsprpcd \
-    vendor/motorola/msm8916-common/proprietary/bin/btnvtool:system/bin/btnvtool \
-    vendor/motorola/msm8916-common/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
+    vendor/motorola/msm8916-common/proprietary/vendor/bin/btnvtool:vendor/bin/btnvtool \
+    vendor/motorola/msm8916-common/proprietary/vendor/bin/hci_qcomm_init:vendor/bin/hci_qcomm_init \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/irsc_util:vendor/bin/irsc_util \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/netmgrd:vendor/bin/netmgrd \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/qmi_motext_hook:vendor/bin/qmi_motext_hook \
