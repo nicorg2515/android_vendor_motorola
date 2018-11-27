780d7be932fe105a2ac7cf0ef1343c125824a7fa
Akianonymus <anonymus.aki@gmail.com>
2018-11-27 19:30:32 +0530

    msm8916-common: Add pm-service from stock

--- msm8916-common/msm8916-common-vendor.mk
+++ msm8916-common/msm8916-common-vendor.mk
@@ -19,6 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/adsprpcd:vendor/bin/adsprpcd \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/irsc_util:vendor/bin/irsc_util \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/netmgrd:vendor/bin/netmgrd \
+    vendor/motorola/msm8916-common/proprietary/vendor/bin/pm-service:vendor/bin/pm-service \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/qmi_motext_hook:vendor/bin/qmi_motext_hook \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/qmuxd:vendor/bin/qmuxd \
     vendor/motorola/msm8916-common/proprietary/vendor/bin/qseecomd:vendor/bin/qseecomd \
